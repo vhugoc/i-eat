31ca57aec55d26471f142dd26648033c5c0986fb
Victor Hugo <vhuugoc@gmail.com>
2020-03-13 17:51:37 -0300

    Criação da migration para categorias dos produtos

---
+++ api/database/migrations/2020_03_13_182756_categories.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Categories extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('user_id')->nullable(false);
+            $table->string('name')->nullable(false);
+            $table->string('color')->nullable(true)->default("uncolored");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+}
