5e6f3b0d2ca532ca18dbfb1dfeb3b9f0a3f65c0b
Victor Hugo <vhuugoc@gmail.com>
2020-03-17 18:00:45 -0300

    Migration de produtos

---
+++ api/database/migrations/2020_03_17_195915_products.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Products extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('user_id')->nullable(false);
+            $table->string('name')->nullable(false);
+            $table->integer('category_id')->nullable(false);
+            $table->string('description')->nullable(true);
+            $table->float('price', 7, 2)->nullable(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
